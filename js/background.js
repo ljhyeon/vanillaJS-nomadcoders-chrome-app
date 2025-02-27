e0c9f4c2d48e8199ebbe282f575c7b20d27558ec
ljhyeon <gee9328@gmail.com>
2025-02-28 00:28:34 +0900

    feat: Add momentum functions using javascript

---
+++ js/background.js
@@ -0,0 +1,13 @@
+const images = [
+    "0.jpeg",
+    "1.jpeg",
+    "2.jpeg",
+];
+const IMG_LEN = images.length;
+
+const chosenImage = images[Math.floor(Math.random() * IMG_LEN)];
+
+const bgImage = document.createElement("img");
+bgImage.src = `img/${chosenImage}`;
+
+document.body.appendChild(bgImage);
---
+++ js/weather.js
@@ -0,0 +1,21 @@
+const API_KEY = config.apikey;
+
+function onGeoOk(position) {
+    const lat = position.coords.latitude;
+    const lng = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const weather = document.querySelector('#weather span:first-child');
+            const city = document.querySelector('#weather span:last-child');
+            const name = data.name;
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${Math.round((data.main.temp - 273.15) * 100) / 100}`;
+        });
+}
+function onGeoError() {
+    alert("Can't find you. No weather for you.");
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
